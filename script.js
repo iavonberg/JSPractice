1f25b826cc9c9900978280d406eec59bd1b6d3ee
Iris Vonberg <irisavonberg@gmail.com>
2023-02-25 15:43:09 -0800

    console logs

---
+++ script.js
@@ -0,0 +1,6 @@
+let myValue = document.getElementById('idOne')
+let info = myValue.textContent;
+console.log(typeof myValue);
+console.log(typeof info);
+console.error(info);
+console.log('%c Hello', 'color:white; background: blue; font-weight: bold');
