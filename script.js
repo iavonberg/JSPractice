38c75c82c14bf881e4650b559aeaa4c489ca43c1
Iris Vonberg <irisavonberg@gmail.com>
2023-02-25 16:13:29 -0800

    query selectors

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const hh = document.querySelector('h1');
 hh.textContent = "New Header";
+const getID = document.getElementById('header');
+getID.textContent = "New Value";
+const byTag = document.getElementsByTagName('li');
+console.log(byTag);
+byTag[4].textContent = "UPDATED ---------";
