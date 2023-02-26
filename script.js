4f2b0c0dd4554670556616172f6362fea0632933
Iris Vonberg <irisavonberg@gmail.com>
2023-02-25 16:24:33 -0800

    More practice with queryselector

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
-const hh = document.querySelector('h1');
-hh.textContent = "New Header";
-const getID = document.getElementById('header');
-getID.textContent = "New Value";
-const byTag = document.getElementsByTagName('li');
-console.log(byTag);
-byTag[4].textContent = "UPDATED ---------";
+// document.write('hello world');
+// console.log('hello');
+// const x = 'Hello World';
+// document.getElementById('idOne').innerHTML = x;
+
+// var myValue = document.getElementById('idOne');
+// var info = myValue.textContent;
+// console.log(typeof myValue);
+// console.log(typeof info);
+// console.info(info);
+// console.warn(info);
+// console.error(info);
+// console.log('%c Hello', 'color:green; font-weight:bold');
+// console.log('%c Hello', 'color:white;background:blue; font-weight:bold');
+
+//console.dir(document);
+// const hh = document.querySelector('#header');
+// hh.textContent = 'New Header';
+
+// const hh = document.querySelector('li');
+// hh.textContent = 'New Header';
+// const getID = document.getElementById('header');
+// getID.textContent = "new Value";
+// const byTag = document.getElementsByTagName("li");
+// console.log(byTag);
+// byTag[4].textContent = "UPDATED ------";
+
+// const el1 = document.querySelector('h1');
+// console.log(el1);
+// const el2 = document.querySelector('.row');
+// console.log(el2);
+// const el3 = document.querySelector('#header');
+// console.log(el3);
+// el1.textContent = "hello";
+// console.log(el3.textContent);
+
+// const myVal = document.querySelectorAll("a[href*=\"JS\"]");
+// console.log(myVal);
+
+// for (let i=0; i<myVal.length;i++) {
+//     console.log(myVal[i].href);
+//     myVal[i].textContent = "Changed " + i;
+//     myVal[i].href = "http://www.discoveryvip.com";
+// }
+
+// const container = document.querySelector("ul.nav");
+// console.log(container);
+// const liActive = container.querySelectorAll("li.active");
+// console.log(liActive)
+// console.log(liActive[0]);
